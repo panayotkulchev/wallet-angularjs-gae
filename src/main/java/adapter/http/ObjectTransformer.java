28ef475ef648dae0edf752aceea6eebacd68abc9
Panayot <panayotkulchev@gmail.com>
2015-06-03 09:07:18 +0300

    date is saved as long in db. ObjTrnsformer is used

---
+++ src/main/java/adapter/http/ObjectTransformer.java
@@ -0,0 +1,51 @@
+package adapter.http;
+
+import core.Expense;
+import core.ExpenseDto;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created on 15-6-2.
+ *
+ * @author Panayot Kulchev <panayotkulchev@gmail.com>
+ */
+
+public class ObjectTransformer {
+
+  public static Expense transformFromDto(ExpenseDto expenseDto) {
+
+    try {
+      Date date = new SimpleDateFormat("dd-MMM-yyyy", Locale.US).parse(expenseDto.date);
+      return new Expense(expenseDto.id, expenseDto.type, expenseDto.amount, date.getTime(), expenseDto.description);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+  public static ExpenseDto transformToDto(Expense expense) {
+
+    String dateAsString = "";
+    String monthNames[] = {"January", "February", "March",
+            "April", "May", "June", "July",
+            "August", "September", "October",
+            "November", "December"};
+
+    Date date = new Date(expense.date);
+    int day = date.getDate();
+    int monthIndex = date.getMonth();
+    int year = date.getYear() + 1900;
+
+    if (day < 11) {
+      dateAsString = "0" + day + "-" + monthNames[monthIndex + 1] + "-" + year;
+    } else {
+      dateAsString = day + "-" + monthNames[monthIndex + 1] + "-" + year;
+    }
+    System.out.println(expense.id + " " + expense.type + " " + expense.amount + " " + dateAsString + " " + expense.description);
+    return new ExpenseDto(expense.id, expense.type, expense.amount, dateAsString, expense.description);
+  }
+
+}
---
+++ src/main/java/core/ExpenseDto.java
@@ -0,0 +1,46 @@
+package core;
+
+import com.vercer.engine.persist.annotation.Key;
+
+/**
+ * Created on 15-6-2.
+ *
+ * @author Panayot Kulchev <panayotkulchev@gmail.com>
+ */
+
+public class ExpenseDto {
+
+  @Key
+  public Long id;
+  public String type;
+  public String amount;
+  public String date;
+  public String description;
+
+  public ExpenseDto() {
+  }
+
+  public ExpenseDto(Long id, String type, String amount, String date, String description) {
+
+    this.id = id;
+    this.type = type;
+    this.amount = amount;
+    this.date = date;
+    this.description = description;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  @Override
+  public String toString() {
+    return "Expense{" +
+            "id=" + id +
+            ", type='" + type + '\'' +
+            ", amount='" + amount + '\'' +
+            ", date='" + date + '\'' +
+            ", description='" + description + '\'' +
+            '}';
+  }
+}
